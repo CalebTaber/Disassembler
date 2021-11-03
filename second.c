c1dab33ff08dd2342b2da25ee3e18affcbd3007b
Caleb Taber <calebtaber@gmail.com>
2021-11-03 18:59:10 -0400

    Updated C code to account for more cases

--- second.c
+++ second.c
@@ -13,3 +13,14 @@ void function_call() {
     printf("Calling return_11()");
     return_11();
 }
+
+void recurse (int c) {
+    if (c == 5) return;
+    else recurse(++c);
+}
+
+void loop() {
+    for (int i = 0; i < 10; i++) {
+        printf("Loop %d\n", i);
+    }
+}
--- second.h
+++ second.h
@@ -3,3 +3,7 @@ void print_hello2();
 int return_11();
 
 void function_call();
+
+void recurse(int c);
+
+void loop();
